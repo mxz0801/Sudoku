38f29d805bd455a9f7320c8c4dbcd0232f9dc3f2
MichaelLy03 <42984000+MichaelLy03@users.noreply.github.com>
2019-05-02 15:08:21 -0700

    Added Timer
    added timer

--- app/src/main/java/com/cecs453/sudoku/MainMenu.java
+++ app/src/main/java/com/cecs453/sudoku/MainMenu.java
@@ -2,12 +2,14 @@ package com.cecs453.sudoku;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +19,14 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser currentUser;
+    private ImageView sudokuLogo;
+    private Chronometer timer;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        timer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,9 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         setContentView(R.layout.activity_main_menu);
         firebaseAuth = FirebaseAuth.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
+        sudokuLogo = findViewById(R.id.logo);
+        sudokuLogo.setImageResource(R.drawable.sudoku_horizontal);
+        timer = findViewById(R.id.timer);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         View headerView = navigationView.getHeaderView(0);
@@ -50,8 +63,10 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_changePass) {
+        if (id == R.id.nav_newgame){
+            newGame();
+        }
+        else if (id == R.id.nav_changePass) {
             startActivity(new Intent(getApplicationContext(),ChangePassword.class));
         } else if (id == R.id.nav_changeProfile) {
             startActivity(new Intent(getApplicationContext(),ChangeProfile.class));
@@ -68,4 +83,8 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    public void newGame(){
+        timer.setBase((SystemClock.elapsedRealtime()));
+    }
 }
+
